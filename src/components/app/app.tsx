a5ea55eea810d9d01baf63e9550ca8d1bb5262bb
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-03-09 01:01:12 +0700

    feat: добавил маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,39 @@
+import { AppRoute } from '../../const/const';
 import { MainPage } from '../../pages/main-page/main-page';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { NotFoundPage } from '../../pages/not-found-page/not-found-page';
+import { FavoritesPage } from '../../pages/favorites-page/favorites-page';
+import { LoginPage } from '../../pages/login-page/login-page';
+import { OfferPage } from '../../pages/offer-page/offer-page';
 
 type AppProps = {
   cityOffersCount: number;
 }
 
 export function App({ cityOffersCount }: AppProps): JSX.Element {
-  return <MainPage cityOffersCount={cityOffersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage cityOffersCount={cityOffersCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.Error}
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>);
 }
